419fdd36854502f0da4343a9f37ba47367c32d62
irene3000 <klitatk.19.96@gmail.com>
2023-02-23 23:52:40 -0500

    actualizando lo de la clase del jueves semana 4

--- index.js
+++ index.js
@@ -19,6 +19,41 @@
 
 // es el div donde estan todos nuestros cards
 // recordemos que el div con la clase challenge tiene el scroll x (horizontal)
+
+
+
+// const challenges = document.querySelector(".challenges");
+
+// // obtener los botones de next y back
+// const btnNext = document.querySelector(".btn-next");
+// const btnBack = document.querySelector(".btn-back");
+
+// btnNext.onclick = function () {
+//   // Esto quiere decir que cuando hagamos un click en el boton con clase .btn-bnext 
+//   // hara lo siguiente
+
+//   // challenges.offsetWidth => nos da el ancho del div challenges
+//   challenges.scrollBy({
+//     left: challenges.offsetWidth / 4,
+//   });
+// };
+
+// btnBack.onclick = function () {
+//   challenges.scrollBy({
+//     left: (challenges.offsetWidth / 4) * -1,
+//   });
+// };
+
+// Esta es formar en la que JS accede a los elementos
+
+// es el div donde estan todos nuestros cards
+// recordemos que el div con la clase challenge tiene el scroll x (horizontal)
+const jsConfetti = new JSConfetti()
+jsConfetti.addConfetti({
+  emojis: ["🇵🇪","#","🎶", "✅", "⭐️"],
+  confettiNumber: 100,
+});
+
 const challenges = document.querySelector(".challenges");
 
 // obtener los botones de next y back
@@ -26,17 +61,28 @@ const btnNext = document.querySelector(".btn-next");
 const btnBack = document.querySelector(".btn-back");
 
 btnNext.onclick = function () {
-  // Esto quiere decir que cuando hagamos un click en el boton con clase .btn-bnext 
+  // Esto quiere decir que cuando hagamos un click en el boton con clase .btn-bnext
   // hara lo siguiente
+  let slider = challenges.offsetWidth / 4;
+
+  if (challenges.offsetWidth <= 480) {
+    slider = challenges.offsetWidth;
+  }
 
   // challenges.offsetWidth => nos da el ancho del div challenges
   challenges.scrollBy({
-    left: challenges.offsetWidth / 4,
+    left: slider,
   });
 };
 
 btnBack.onclick = function () {
+  let slider = (challenges.offsetWidth / 4) * -1;
+
+  if (challenges.offsetWidth <= 480) {
+    slider = -challenges.offsetWidth;
+  }
+
   challenges.scrollBy({
-    left: (challenges.offsetWidth / 4) * -1,
+    left: slider,
   });
 };
